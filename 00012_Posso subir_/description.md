1293183f260e9cec6c5f9bb4a8df225b20df941f
rocio@mumuki.org <rocio@mumuki.org>
2022-08-22 21:20:10 +0000

    Mumuki Export on 2022-08-22 21:20:10 +0000

---
+++ 00012_Posso subir_/description.md
@@ -0,0 +1,14 @@
+Em um parque de diversões da cidade eles instalaram uma nova montanha russa :roller_coaster: e eles nos pediram para ajudar a dizer às pessoas se elas podem entrar ou não antes de irem para a fila. Os requisitos para entrar na atração são:
+
+* Alcançar a altura mínima de 1.5m (ou 1.2m se está acompanhada por um adulto).
+* Não ter problemas cardíacos.
+
+> Defina a função de 3 parâmetros `pode_subir` que recebe a altura de uma pessoa em metros, se está acompanhada por um adulto e se tem algum problema cardíaco. Exemplo:
+>
+> ```python
+> ム pode_subir(1.7, False, True)
+> False # não pode subir
+>   	# porque embora tenha mais de 1.5m,
+>   	# tem um problema cardíaco
+> ```
+
--- 00012_Posso subir_/test.py
+++ 00012_Posso subir_/test.py
@@ -1,16 +1,16 @@
   
   def test_una_persona_que_mide_1_5m_no_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_puede_subirse_a_la_atraccion(self):
-    self.assertTrue(puede_subirse(1.5, False, False))
+    self.assertTrue(pode_subir(1.5, False, False))
   
   def test_una_persona_que_mide_1_7m_no_esta_acompaniada_por_un_adulto_y_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
-    self.assertFalse(puede_subirse(1.7, False, True))
+    self.assertFalse(pode_subir(1.7, False, True))
   
   def test_una_persona_que_mide_1_2m_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_puede_subirse_a_la_atraccion(self):
-    self.assertTrue(puede_subirse(1.2, True, False))
+    self.assertTrue(pode_subir(1.2, True, False))
   
   def test_una_persona_que_mide_1_2m_no_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
-    self.assertFalse(puede_subirse(1.2, False, False))
+    self.assertFalse(pode_subir(1.2, False, False))
   
   def test_una_persona_que_mide_1_1m_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
-    self.assertFalse(puede_subirse(1.1, True, False))
+    self.assertFalse(pode_subir(1.1, True, False))
   
