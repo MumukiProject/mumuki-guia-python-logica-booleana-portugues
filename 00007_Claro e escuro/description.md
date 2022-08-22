9c43cdcd3fc384a4db2306b4cba15936ccea23f6
rocio@mumuki.org <rocio@mumuki.org>
2022-08-22 20:50:24 +0000

    Mumuki Export on 2022-08-22 20:50:24 +0000

---
+++ 00007_Claro e escuro/description.md
@@ -0,0 +1,14 @@
+Oli está projetando uma página web e está se concentrando no contraste dos componentes. :star_struck:
+Para conseguir ele pediu que definíssemos a função `tem_contraste` que recebe como argumentos a cor da letra e a cor do fundo da página e retorna se a página tem contraste.
+
+Para começar já temos a função `e_tom_claro` que utiliza uma cor por parâmetro e retorna se é claro.
+
+``` javascript
+ム e_tom_claro('vermelho')
+False
+
+ム e_tom_claro('branco')
+True
+```
+
+> Agora é a sua vez! Defina a função `tem_contraste`. Para que a página tenha contraste é necessário ter o fundo claro e a letra não ou  ter a letra clara e o fundo não.:relieved:
--- 00007_Claro e escuro/test.py
+++ 00007_Claro e escuro/test.py
@@ -1,12 +1,12 @@
   
   def test_tiene_contraste_blanco_negro_es_verdadero(self):
-    self.assertTrue(tiene_contraste("blanco","negro"))
+    self.assertTrue(tem_contraste("blanco","negro"))
 
   def test_tiene_contraste_beige_rosa_es_falso(self):
-    self.assertFalse(tiene_contraste("beige","rosa"))
+    self.assertFalse(tem_contraste("beige","rosa"))
 
   def test_tiene_contraste_rojo_azul_es_falso(self):
-    self.assertFalse(tiene_contraste("rojo","azul"))
+    self.assertFalse(tem_contraste("rojo","azul"))
 
   def test_tiene_contraste_negro_amarillo_es_verdadero(self):
-    self.assertTrue(tiene_contraste("negro","amarillo"))
+    self.assertTrue(tem_contraste("negro","amarillo"))
