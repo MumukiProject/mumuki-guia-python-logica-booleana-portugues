a780c62c6d8e8b1c7e67a65fbd8b2161b7f33329
rocio@mumuki.org <rocio@mumuki.org>
2022-08-23 15:44:24 +0000

    Mumuki Export on 2022-08-23 15:44:24 +0000

--- 00002_Negar não custa nada/test.py
+++ 00002_Negar não custa nada/test.py
@@ -16,17 +16,17 @@
   
   
   def test_alguien_de_20_anios_no_es_menor_de_edad(self):
-    self.assertFalse(e_maior_de_idade(20))
+    self.assertFalse(e_menor_de_idade(20))
   
   
   def test_alguien_de_18_anios_no_es_menor_de_edad(self):
-    self.assertFalse(e_maior_de_idade(18))
+    self.assertFalse(e_menor_de_idade(18))
   
   
   def test_alguien_de_17_anios_es_menor_de_edad(self):
-    self.assertTrue(e_maior_de_idade(17))
+    self.assertTrue(e_menor_de_idade(17))
   
   
   def test_alguien_de_16_anios_es_menor_de_edad(self):
-    self.assertTrue(e_maior_de_idade(16))
+    self.assertTrue(e_menor_de_idade(16))
   
