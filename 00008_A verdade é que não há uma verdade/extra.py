9f11dee8dc9f4c72ce3b4710b6e7982cc1a6b3f2
rocio@mumuki.org <rocio@mumuki.org>
2022-08-23 16:02:53 +0000

    Mumuki Export on 2022-08-23 16:02:53 +0000

--- 00008_A verdade é que não há uma verdade/extra.py
+++ 00008_A verdade é que não há uma verdade/extra.py
@@ -3,5 +3,5 @@ colores_claros = ["blanco", "beige", "celeste", "rosa", "amarillo"]
 def es_tono_claro(color):
   return color in colores_claros
   
-def tiene_contraste(letra, fondo):
+def tem_contraste(letra, fondo):
   return es_tono_claro(letra) != es_tono_claro(fondo)
