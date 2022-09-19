1d93497818382ba9b74f3edccb5a28dcc6b4c4ad
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 20:11:45 +0000

    Mumuki Export on 2022-09-19 20:11:45 +0000

--- 00003_Peripatéticas/test.py
+++ 00003_Peripatéticas/test.py
@@ -1,20 +1,20 @@
   
-  def test_es_necesario_que_filosofia_lleve_tilde_y_grecia_esté_en_mayusculas_para_saber_si_la_persona_es_peripatetica(self):
+  def test_e_necessario_que_grecia_tenha_acento_e_esteja_em_letras_maiusculas_para_saber_se_a_pessoa_e_peripatetica(self):
     self.assertFalse(e_peripatetica("filosofia", "Grecia", 3))
-    self.assertTrue(e_peripatetica("filosofía", "Grecia", 3))
+    self.assertTrue(e_peripatetica("filosofia", "Grécia", 3))
   
-  def test_una_persona_que_se_desempena_en_filosofia_es_de_grecia_y_camina_3kms_al_dia_es_peripatetica(self):
+  def test_uma_pessoa_que_tem_bom_desempenho_em_filosofia_e_da_grecia_e_caminha_3km_por_dia_e_peripatetica(self):
     self.assertTrue(e_peripatetica("filosofia", "Grecia", 3) or e_peripatetica("filosofía", "Grecia", 3) or e_peripatetica("filosofía", "grecia", 3) or e_peripatetica("filosofia", "grecia", 3))
   
-  def test_una_persona_que_se_desempena_en_filosofia_es_de_grecia_y_camina_2kms_al_dia_no_es_peripatetica(self):
+  def test_uma_pessoa_que_tem_bom_desempenho_em_filosofia_e_da_grecia_e_caminha_2km_por_dia_nao_e_peripatetica(self):
     self.assertFalse((e_peripatetica("filosofia", "Grecia", 2) or e_peripatetica("filosofía", "Grecia", 2)))
   
-  def test_una_persona_que_se_desempena_en_filosofia_es_de_argentina_y_camina_5kms_al_dia_no_es_peripatetica(self):
+  def test_uma_pessoa_que_tem_bom_desempenho_em_filosofia_e_da_argentina_e_caminha_5km_por_dia_nao_e_peripatetica(self):
     self.assertFalse(e_peripatetica("filosofía", "Argentina", 5))
   
-  def test_una_persona_que_se_desempena_en_atletismo_es_de_grecia_y_camina_10kms_al_dia_no_es_peripatetica(self):
+  def test_uma_pessoa_que_tem_bom_desempenho_em_atletismo_e_da_grecia_e_caminha__10km_por_dia_nao_e_peripatetica(self):
     self.assertFalse(e_peripatetica("atleta", "Grecia", 10))
   
-  def test_una_persona_que_se_desempena_en_ingenieria_es_de_colombia_y_camina_1kms_al_dia_no_es_peripatetica(self):
+  def test_uma_pessoa_que_tem_bom_desempenho_em_engenharia_e_da_colombia_e_caminha_1km_por_dia_nao_e_peripatetica(self):
     self.assertFalse(e_peripatetica("profesor", "Colombia", 1))
   
