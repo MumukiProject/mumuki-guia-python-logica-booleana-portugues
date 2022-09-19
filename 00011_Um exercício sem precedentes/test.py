081fe255a7b86ccfc848b3385238bb03c142e2b1
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 20:32:57 +0000

    Mumuki Export on 2022-09-19 20:32:57 +0000

--- 00011_Um exercício sem precedentes/test.py
+++ 00011_Um exercício sem precedentes/test.py
@@ -1,18 +1,18 @@
  
-  def test_se_puede_concentrar_te_95_true_es_verdadero(self):
-    self.assertTrue(pode_se_concentrar("té",95, True))
+  def test_e_possivel_concentrar_cha_95_true_e_verdadeiro(self):
+    self.assertTrue(pode_se_concentrar("chá",95, True))
 
-  def test_se_puede_concentrar_té_100_true_es_verdadero(self):
-    self.assertTrue(pode_se_concentrar("té",100, True))
+  def test_e_possivel_concentrar_cha_100_true_e_verdadeiro(self):
+    self.assertTrue(pode_se_concentrar("chá",100, True))
 
-  def test_se_puede_concentrar_mate_80_true_es_verdadero(self):
-    self.assertTrue(pode_se_concentrar('mate', 80, True))
+  def test_e_possivel_concentrar_chimarrao_80_true_e_verdadeiro(self):
+    self.assertTrue(pode_se_concentrar('chimarrão', 80, True))
 
-  def test_se_puede_concentrar_mate_70_true_es_falso(self):
-    self.assertFalse(pode_se_concentrar('mate', 70, True))
+  def test_e_possivel_concentrar_chimarrao_70_true_e_falso(self):
+    self.assertFalse(pode_se_concentrar('chimarrão', 70, True))
 
-  def test_se_puede_concentrar_t_94_true_es_falso(self):
-    self.assertFalse(pode_se_concentrar("té",94, True))
+  def test_E_possivel_concentrar_cha_94_true_e_falso(self):
+    self.assertFalse(pode_se_concentrar("chá",94, True))
 
-  def test_se_puede_concentrar_té_95_false_es_falso(self):
-    self.assertFalse(pode_se_concentrar("té",95, False))
+  def test_e_possivel_concentrar_cha_95_false_e_falso(self):
+    self.assertFalse(pode_se_concentrar("chá",95, False))
