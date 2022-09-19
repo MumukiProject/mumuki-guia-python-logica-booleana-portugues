bb2163771adca7678746543371bfdd496615b00f
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 20:16:08 +0000

    Mumuki Export on 2022-09-19 20:16:08 +0000

--- 00006_E agora___ quem poderá nos ajudar_/test.py
+++ 00006_E agora___ quem poderá nos ajudar_/test.py
@@ -1,24 +1,24 @@
   
-  def test_un_lunes_feriado_a_las_14hs_el_banco_no_esta_abierto(self):
+  def test_uma_segunda_feriado_as_14hs_o_banco_nao_esta_aberto(self):
     self.assertFalse(esta_aberto(True, "lunes", 14))
   
   
-  def test_un_miercoles_feriado_a_las_20hs_el_banco_no_esta_abierto(self):
+  def test_uma_quarta_feriado_as_20hs_o_banco_nao_esta_aberto(self):
     self.assertFalse(esta_aberto(True, "miércoles", 20))
   
   
-  def test_un_jueves_corriente_a_las_13hs_el_banco_esta_abierto(self):
+  def test_uma_quinta_comum_as_13hs_o_banco_esta_aberto(self):
     self.assertTrue(esta_aberto(False, "jueves", 13))
   
   
-  def test_un_sabado_corriente_a_las_11hs_el_banco_no_esta_abierto(self):
+  def test_um_sabado_comum_as_11hs_o_banco_nao_esta_aberto(self):
     self.assertFalse(esta_aberto(False, "sábado", 11))
   
   
-  def test_un_domingo_corriente_a_las_19hs_el_banco_no_esta_abierto(self):
+  def test_um_domingo_comum_as_19hs_o_banco_nao_esta_aberto(self):
     self.assertFalse(esta_aberto(False, "domingo", 19))
   
   
-  def test_un_martes_corriente_a_las_16hs_el_banco_no_esta_abierto(self):
+  def test_uma_terca_comum_as_16hs_o_banco_nao_esta_aberto(self):
     self.assertFalse(esta_aberto(False, "martes", 16))
   
