9b820098c739f16cfa741863b13b09ae97ae79ba
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 20:24:03 +0000

    Mumuki Export on 2022-09-19 20:24:03 +0000

--- 00009_Olá! Meu nome é Xor/test.py
+++ 00009_Olá! Meu nome é Xor/test.py
@@ -1,16 +1,16 @@
   
-  def test_la_disyuncion_logica_excluyente_entre_True_y_True_da_False(self):
+  def test_a_disjuncao_logica_exclusiva_entre_True_e_True_da_False(self):
     self.assertFalse(xor(True, True))
   
   
-  def test_la_disyuncion_logica_excluyente_entre_True_y_False_da_True(self):
+  def test_a_disjuncao_logica_exclusiva_entre_True_e_False_da_True(self):
     self.assertTrue(xor(True, False))
   
   
-  def test_la_disyuncion_logica_excluyente_entre_False_y_True_da_True(self):
+  def test_a_disjuncao_logica_exclusiva_entre_False_e_True_da_True(self):
     self.assertTrue(xor(False, True))
   
   
-  def test_la_disyuncion_logica_excluyente_entre_False_y_False_da_False(self):
+  def test_a_disjuncao_logica_exclusiva_entre_False_e_False_da_False(self):
     self.assertFalse(xor(False, False))
   
