76c124ccded510f1bcb3cdf9329a25996c0d0e42
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 20:22:14 +0000

    Mumuki Export on 2022-09-19 20:22:14 +0000

--- 00007_Claro e escuro/test.py
+++ 00007_Claro e escuro/test.py
@@ -2,11 +2,11 @@
   def test_tem_contraste_branco_preto_e_verdadeiro(self):
     self.assertTrue(tem_contraste("branco","preto"))
 
-  def test_tiene_contraste_beige_rosa_es_falso(self):
-    self.assertFalse(tem_contraste("beige","rosa"))
+  def test_tem_contraste_bege_rosa_e_falso(self):
+    self.assertFalse(tem_contraste("bege","rosa"))
 
-  def test_tiene_contraste_rojo_azul_es_falso(self):
-    self.assertFalse(tem_contraste("rojo","azul"))
+  def test_tem_contraste_vermelho_azul_é_falso(self):
+    self.assertFalse(tem_contraste("vermelho","azul"))
 
-  def test_tiene_contraste_negro_amarillo_es_verdadero(self):
-    self.assertTrue(tem_contraste("negro","amarillo"))
+  def test_tem_contraste_preto_amarelo_é_verdadeiro(self):
+    self.assertTrue(tem_contraste("preto","amarelo"))
