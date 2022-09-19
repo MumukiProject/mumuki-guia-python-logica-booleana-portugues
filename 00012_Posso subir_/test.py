7ff1e02338b4e1fcc2248e8452edfdecb73011f2
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 20:38:05 +0000

    Mumuki Export on 2022-09-19 20:38:05 +0000

--- 00012_Posso subir_/test.py
+++ 00012_Posso subir_/test.py
@@ -1,16 +1,16 @@
   
-  def test_una_persona_que_mide_1_5m_no_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_puede_subirse_a_la_atraccion(self):
+  def test_uma_pessoa_que_mede_1_5m_nao_esta_acompanhada_por_um_adulto_e_nao_tem_problemas_cardiacos_pode_subir_para_a_atracao(self):
     self.assertTrue(pode_subir(1.5, False, False))
   
-  def test_una_persona_que_mide_1_7m_no_esta_acompaniada_por_un_adulto_y_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
+  def test_uma_pessoa_que_mede_1_7m_nao_esta_acompanhada_por_um_adulto_e_tem_problemas_cardiacos_nao_pode_subir_para_a_atracao(self):
     self.assertFalse(pode_subir(1.7, False, True))
   
-  def test_una_persona_que_mide_1_2m_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_puede_subirse_a_la_atraccion(self):
+  def test_uma_pessoa_que_mede_1_2m_esta_acompanhada_por_um_adulto_e_nao_tem_problemas_cardiacos_pode_subir_para_a_atracao(self):
     self.assertTrue(pode_subir(1.2, True, False))
   
-  def test_una_persona_que_mide_1_2m_no_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
+  def test_uma_pessoa_que_mede_1_2m_nao_esta_acompanhada_por_um_adulto_e_nao_tem_problemas_cardiacos_nao_pode_subir_para_a_atracao(self):
     self.assertFalse(pode_subir(1.2, False, False))
   
-  def test_una_persona_que_mide_1_1m_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
+  def test_uma_pessoa_que_mede_1_1m_esta_acompanhada_por_um_adulto_e_nao_tem_problemas_cardiacos_nao_pode_subir_para_a_atracao(self):
     self.assertFalse(pode_subir(1.1, True, False))
   
