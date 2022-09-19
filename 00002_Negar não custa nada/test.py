d94a0ed8cd1310c3cd43b102820e6b919969f8c1
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 20:03:51 +0000

    Mumuki Export on 2022-09-19 20:03:51 +0000

--- 00002_Negar não custa nada/test.py
+++ 00002_Negar não custa nada/test.py
@@ -1,32 +1,32 @@
   
-  def test_alguien_de_20_anios_es_mayor_de_edad(self):
+  def test_alguem_de_20_anos_e_maior_de_idade(self):
     self.assertTrue(e_maior_de_idade(20))
   
   
-  def test_alguien_de_18_anios_es_mayor_de_edad(self):
+  def test_alguem_de_18_anos_e_maior_de_idade(self):
     self.assertTrue(e_maior_de_idade(18))
   
   
-  def test_alguien_de_17_anios_no_es_mayor_de_edad(self):
+  def test_alguem_de_17_anos_nao_e_maior_de_idade(self):
     self.assertFalse(e_maior_de_idade(17))
   
   
-  def test_alguien_de_16_anios_no_es_mayor_de_edad(self):
+  def test_alguem_de_16_anos_nao_e_maior_de_idade(self):
     self.assertFalse(e_maior_de_idade(16))
   
   
-  def test_alguien_de_20_anios_no_es_menor_de_edad(self):
+  def test_alguem_de_20_anos_nao_e_menor_de_idade(self):
     self.assertFalse(e_menor_de_idade(20))
   
   
-  def test_alguien_de_18_anios_no_es_menor_de_edad(self):
+  def test_alguem_de_18_anos_nao_e_menor_de_idade(self):
     self.assertFalse(e_menor_de_idade(18))
   
   
-  def test_alguien_de_17_anios_es_menor_de_edad(self):
+  def test_alguem_de_17_anos_e_menor_de_idade(self):
     self.assertTrue(e_menor_de_idade(17))
   
   
-  def test_alguien_de_16_anios_es_menor_de_edad(self):
+  def test_alguem_de_16_anos_e_menor_de_idade(self):
     self.assertTrue(e_menor_de_idade(16))
   
