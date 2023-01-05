33d64f400dc428d402b56da3209e247e8da98032
nadia@mumuki.org <nadia@mumuki.org>
2023-01-05 22:03:36 +0000

    Mumuki Export on 2023-01-05 22:03:36 +0000

--- 00003_Peripatéticas/test.py
+++ 00003_Peripatéticas/test.py
@@ -4,10 +4,10 @@
     self.assertTrue(e_peripatetica("filosofia", "Grécia", 3))
   
   def test_uma_pessoa_que_tem_bom_desempenho_em_filosofia_é_da_grécia_e_caminha_3km_por_dia_é_peripatética(self):
-    self.assertTrue(e_peripatetica("filosofia", "Grécia", 3) or e_peripatetica("filosofía", "Grécia", 3))
+    self.assertTrue(e_peripatetica("filosofia", "Grécia", 3) or e_peripatetica("filosofia", "Grecia", 3))
   
   def test_uma_pessoa_que_tem_bom_desempenho_em_filosofia_é_da_grécia_e_caminha_2km_por_dia_não_é_peripatética(self):
-    self.assertFalse((e_peripatetica("filosofia", "Grécia", 2) or e_peripatetica("filosofía", "Grécia", 2)))
+    self.assertFalse((e_peripatetica("filosofia", "Grécia", 2) or e_peripatetica("filosofia", "Grecia", 2)))
   
   def test_uma_pessoa_que_tem_bom_desempenho_em_filosofia_é_da_argentina_e_caminha_5km_por_dia_não_é_peripatética(self):
     self.assertFalse(e_peripatetica("filosofia", "Argentina", 5))
